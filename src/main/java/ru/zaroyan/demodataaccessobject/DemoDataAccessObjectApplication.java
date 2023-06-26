bcbbdeae5eb6d7f9afcca47ee46664755c5727e3
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-06-26 13:37:05 +0300

    "initial commit"

---
+++ src/main/java/ru/zaroyan/demodataaccessobject/DemoDataAccessObjectApplication.java
@@ -0,0 +1,13 @@
+package ru.zaroyan.demodataaccessobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataAccessObjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDataAccessObjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zaroyan/demodataaccessobject/DemoDataAccessObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zaroyan.demodataaccessobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDataAccessObjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
